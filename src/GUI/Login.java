faa65e64ac2e37ca66495262dd7bd0cf7a2b42e4
Umar Mukhtar <umar@tuffah.info>
2015-08-13 16:32:03 +0800

    baiki alert CIS - stamp 13082015.1632

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -182,7 +182,7 @@ public class Login extends javax.swing.JFrame {
             }
         });
 
-        jLabel6.setText("Version 16.10");
+        jLabel6.setText("Version 16.11");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
